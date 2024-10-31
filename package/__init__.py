9034b0be4712755a1bf0e1d86673fe76fc114237
talibsayyed <talibsayyed1999@gmail.com>
2024-10-31 15:59:11 +0530

    This is the first commit

---
+++ package/__init__.py
@@ -0,0 +1,1 @@
+## __init__.py is a special file used in python to define packages and initialize their namespaces
---
+++ test.py
@@ -0,0 +1,5 @@
+from package.maths import *
+from package.subpackage.mult import multiply
+print(addition(5,6))
+print(substraction(6,5))
+print(multiply(5,6))
